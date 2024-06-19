ecfc86fd4bd8005557a3d944bb61da1cdd99c794
hesamameri <hesam7991ameri@gmail.com>
2024-06-19 14:11:13 +0330

    setting up auth

--- database.py
+++ database.py
@@ -1,11 +1,16 @@
 # database.py
+import os
 from sqlalchemy import create_engine, MetaData
 from databases import Database
+from dotenv import load_dotenv
 
-DATABASE_URL = "postgresql://user:password@localhost/dbname"
+load_dotenv()  # Load environment variables from .env file
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 database = Database(DATABASE_URL)
 metadata = MetaData()
 
 engine = create_engine(DATABASE_URL)
 metadata.create_all(engine)
+
